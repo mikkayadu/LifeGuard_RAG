3fc9f86d7dc8ce7be477dcf5e4af6897eef2e052
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-06-12 10:55:27 +0000

    Add CORS

--- app/main.py
+++ app/main.py
@@ -2,19 +2,20 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes.upload import router as upload_router
 from app.routes.ask import router as ask_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Health And Fitness Assistant",
     description="Ask questions about uploaded health PDF reports.",
     version="1.0.0"
 )
-
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
+CORSMiddleware,
+    allow_origins=["https://lifeguard-vq69.onrender.com/", "http://localhost:80/docs"],  
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
+
 app.include_router(upload_router, prefix="/api")
 app.include_router(ask_router, prefix="/api")
